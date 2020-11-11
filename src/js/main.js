6e376b29b6acc22e71a899b4288009a61b6f603c
Крылов Антон Юрьевич <krylov.au@kontur>
2020-11-11 23:25:28 +0500

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+import "./slider";
+import modals from './modules/modals';
+/*
+Есть определенный триггер, дальше идет показ модального окна, используем определенный селектор. 
+
+
+*/
+
+window.addEventListener('DOMContentLoaded', () => {
+    modals();
+});
+
+
+console.log(1);
